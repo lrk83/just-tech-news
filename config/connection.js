0f8e24c7532ed96b3d657225ee7b8012c87aa879
Lukas Keel <lrk83@cornell.edu>
2021-07-29 15:31:08 -0400

    api user routes working

---
+++ config/connection.js
@@ -0,0 +1,11 @@
+const Sequelize = require('sequelize');
+
+require('dotenv').config();
+
+const sequelize = new Sequelize(process.env.DB_NAME, 'root', 'MySQLy76y76y76', {
+    host: 'localhost',
+    dialect: 'mysql',
+    port: 3306
+});
+
+module.exports = sequelize;
